17925f967d93d422e2b308d5beb69418e2c204b9
cheng924 <1961639211@qq.com>
2025-03-14 10:15:43 +0800

    灵动词幕功能

---
+++ app/src/main/java/com/example/jvsglass/activities/FileItem.kt
@@ -0,0 +1,7 @@
+package com.example.jvsglass.activities
+
+data class FileItem(
+    val fileName: String,
+    val fileDate: String,
+    val fileContent: String
+)
--- app/src/main/java/com/example/jvsglass/activities/TelepromptActivity.kt
+++ app/src/main/java/com/example/jvsglass/activities/TelepromptActivity.kt
@@ -1,26 +1,40 @@
 package com.example.jvsglass.activities;
 
 import android.os.Bundle;
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jvsglass.R
+import com.example.jvsglass.utils.ToastUtils
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.jvsglass.R;
-
-public class TelepromptActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+class TelepromptActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_teleprompt);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        val functions = listOf(
+            FileItem("sdhiasigoasjgsfhaogkjksdgjklzxcmisjdkgsdgasdg", "2025/3/4 14:00", "sdhijiasdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsdhijiasgdasjgdsgdasjgd"),
+            FileItem("xniujnjnjdnjg", "2025/3/8 15:00", "xniujnjnjdnjg")
+        )
+
+        val recyclerView: RecyclerView = findViewById(R.id.rvFiles)
+        recyclerView.layoutManager = GridLayoutManager(this, 1)
+        recyclerView.adapter = FileAdapter(functions)
+
+        findViewById<ImageView>(R.id.btnBack).setOnClickListener {
+            ToastUtils.show(this, "返回")
+            finish() // 关闭当前Activity，回到上一个Activity
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+        }
+
+        findViewById<LinearLayout>(R.id.btnNew).setOnClickListener {
+            ToastUtils.show(this, "新建文本")
+        }
+
+        findViewById<LinearLayout>(R.id.btnImport).setOnClickListener {
+            ToastUtils.show(this, "导入文本")
+        }
     }
 }
